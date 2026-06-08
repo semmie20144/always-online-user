c047f0b5729596857d512ce0799905dc8bc5b948
semmie20144 <165664486+semmie20144@users.noreply.github.com>
2026-06-08 22:53:02 +0200

    adding files to let it work

---
+++ index.js
@@ -0,0 +1,15 @@
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js')
+
+// Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+bot.on("ready", () => {
+  bot.editStatus("dnd");
+});
+
+bot.connect(); // Get the bot to connect to Discord
